607eac7f47f0dbee2d73bcc71c85b3a7d86368ea
杨俊 <15818708414@163.com>
2017-03-26 15:41:55 +0800

    Jun

--- answer/Home/Controller/StuController.class.php
+++ answer/Home/Controller/StuController.class.php
@@ -28,6 +28,14 @@ class StuController extends Controller
     // 学生提问提交处理
     public function askAct()
     {
+        // 接受参数
+        $title = I('post.title');
+        $content = I('post.content');
+        $keys = rtrim(I('post.keys') , ','); // 1,2,3,4,
+
+        // 剥离标签处理(预留)
+
+        // 插入数据库
         
     }
 
